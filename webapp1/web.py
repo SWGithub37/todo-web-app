39282bd3e9d0c850af78e244e3507191f4b5a326
sharon <sharon.netmail@yahoo.com>
2024-04-14 13:01:15 +0300

    Remove txt file from Files folder

--- webapp1/web.py
+++ webapp1/web.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos('Files/toto.txt')
+todos = functions.get_todos()
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
-    functions.write_todos(todos, 'Files/toto.txt')
+    functions.write_todos(todos)
 
 
 st.title("My Todo App")
@@ -18,7 +18,7 @@ for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
-        functions.write_todos(todos, 'Files/toto.txt')
+        functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
 
